4cac1482a26e4607caeead7435106d78eabc1698
Efe Emir Pekmez <efeemir103@gmail.com>
2019-04-11 23:11:21 +0300

    learning model fields

--- django-study/src/products/models.py
+++ django-study/src/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    descripton = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='this is so cool! :D')
+    title = models.CharField(max_length=120) # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField(default='Default text')
