7a33af6bcd57685e3ae8244678e77eded14ea39c
Efe Emir Pekmez <efeemir103@gmail.com>
2019-04-11 23:33:02 +0300

    learning models...

--- django-study/src/products/models.py
+++ django-study/src/products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(default='Default text')
+    featured = models.BooleanField(default=True)
