b7fb3dec9a9805e2210a5f21412ef81a02bf54dc
efeemir103 <efeemir103@gmail.com>
2019-05-28 01:20:59 +0300

    Changed forms...

--- django-study/src/products/forms.py
+++ django-study/src/products/forms.py
@@ -11,3 +11,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Enter your title..."}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+                attrs={
+                    "placeholder":"Enter your description...",
+                    "class":"new-class-name two",
+                    "id":"my-id-for-textarea",
+                    "rows":20,
+                    "cols":120
+                }
+            )
+        )
+    price = forms.DecimalField(initial=199.99)
--- django-study/src/products/views.py
+++ django-study/src/products/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 
-from .forms import ProductForm
+from .forms import RawProductForm
 
 from .models import Product
 # Create your views here.
 def product_create_view(request,*args, **kwargs): # *args, **kwargs
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-
+    my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            # now the data is good
+            print(my_form.cleaned_data) # Data as dict
+            Product.objects.create(**my_form.cleaned_data) # Slicing dict aas arguments and creating from data
+        else:
+            print(my_form.errors)
     context = {
-        'form':form
+        'form':my_form
     }
     return render(request,"product_create.html",context)
 
