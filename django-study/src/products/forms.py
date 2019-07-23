7c58cb2cf0b10ab809c229701a86387ffa75c80a
efeemir103 <efeemir103@gmail.com>
2019-07-23 20:20:09 +0300

    Form Validation Mehtods

--- django-study/src/products/forms.py
+++ django-study/src/products/forms.py
@@ -4,6 +4,20 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='',widget=forms.TextInput(attrs={"placeholder":"Your title"})) 
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+                attrs={
+                    "placeholder":"Enter your description...",
+                    "class":"new-class-name two",
+                    "id":"my-id-for-textarea",
+                    "rows":20,
+                    "cols":120
+                }
+            )
+        )
+    price = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = [
@@ -11,6 +25,15 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+    
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "good" in title: # Check if title field contains word "good"
+            return title
+        if title.endswith("lol"):
+            return title
+        else:
+            raise forms.ValidationError("This is not a valid title")
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Enter your title..."}))
--- django-study/src/products/views.py
+++ django-study/src/products/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
 
-from .forms import RawProductForm
+from .forms import ProductForm, RawProductForm
 
 from .models import Product
 # Create your views here.
-def product_create_view(request,*args, **kwargs): # *args, **kwargs
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data) # Data as dict
-            Product.objects.create(**my_form.cleaned_data) # Slicing dict aas arguments and creating from data
-        else:
-            print(my_form.errors)
+
+# def product_create_view(request,*args, **kwargs): # *args, **kwargs
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data) # Data as dict
+#             Product.objects.create(**my_form.cleaned_data) # Slicing dict aas arguments and creating from data
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         'form':my_form
+#     }
+#     return render(request,"product_create.html",context)
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    
     context = {
-        'form':my_form
+        'form': form
     }
-    return render(request,"product_create.html",context)
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request,*args, **kwargs): # *args, **kwargs
     obj = Product.objects.get(id=1)
     context = {
     "object":obj
     }
-    return render(request,"product_detail.html",context)
+    return render(request,"products/product_detail.html",context)
