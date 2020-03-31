9507ea920a56044c4a45cf0c3a024b9ba1fe6817
efeemir103 <efeemir103@gmail.com>
2020-03-31 17:25:52 +0300

    Added commens.

--- svelte-scoreboard/src/App.svelte
+++ svelte-scoreboard/src/App.svelte
@@ -1,12 +1,18 @@
 <script>
+	// Import custom Svelte components.
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
 	import AddPlayer from './AddPlayer.svelte';
+
+	// An object array containing users.
 	let players = [];
+
+	// Use fetch api to get 10 random users.
 	fetch('https://randomuser.me/api/?results=10')
 		.then(response => {
 			if(response.status === 200){
 				response.json().then(data => {
+					// Manipulate each object in returned data so there is only name and points fields.
 					players = data["results"].map(o => {
 						let newObj = {
 							name: o["name"]["first"] + " " + o["name"]["last"],
@@ -19,6 +25,7 @@
     	})
     	.catch(err => {
 			console.log('Fetch Error: ', err);
+			// If couldn't fetch data initiate players with default data.
 			players = [
 				{
 					name: "John Doe",
@@ -35,23 +42,31 @@
 			];
 		  });
 	
+	// Handle addplayer event.
 	const addPlayer = (e) => {
 		const newPlayer = e.detail;
 		console.log(newPlayer);
 		players = [...players, newPlayer];
 	};
+
+	// Handle removeplayer event.
 	const removePlayer = (e) => {
 		players = players.filter(player => player.name !== e.detail);
 	}
 </script>
 
+<!-- Custom component "Navbar.svelte" -->
 <Navbar />
 <div class="container">
+	<!-- Custom component "AddPlayer.svelte" -->
 	<AddPlayer on:addplayer={addPlayer} />
+	<!-- Conditional html elements -->
 	{#if players.length === 0}
 		<p>No Players</p>
 	{:else}
+		<!-- Loopthrough each player -->
 		{#each players as player}
+			<!-- Custom component "Player.svelte" -->
 			<Player name={player.name} points={player.points} on:removeplayer={removePlayer}/>
 		{/each}
 	{/if}
--- svelte-scoreboard/src/Player.svelte
+++ svelte-scoreboard/src/Player.svelte
@@ -1,17 +1,23 @@
 <script>
+    // Use Event Dispatcher to send an event to main process.
     import { createEventDispatcher } from 'svelte';
 
+    // Create a event dispatcher.
     const dispatch = createEventDispatcher();
     
+    // Load props passed in.
     export let name;
     export let points;
+    // Show point changing buttons.
 	let showControls = false;
 
+    // Handle on:click events.
 	const addPoint = () => points++;
 	const removePoint = () => points--;
     const toggleControls = () => showControls = !showControls;
     const onDelete = () => dispatch("removeplayer", name);
 </script>
+
 <style>
 	h1 {
 		color: #204f6e;
@@ -22,15 +28,18 @@
 	}
 </style>
 
+<!-- Player Card -->
 <div class="card">
     <h1>
         {name}
         <button class="btn btn-sm" on:click={toggleControls}>
+            <!-- Switch in between "-" and "+" -->
             {#if showControls}-{:else}+{/if}
         </button>
         <button class="btn btn-danger" on:click={onDelete}>x</button>
     </h1>
     <h3>Points: {points}</h3>
+    <!-- Control section -->
     {#if showControls}
     <button class="btn" on:click={addPoint}>+1</button>
     <button class="btn btn-dark" on:click={removePoint}>-1</button>
